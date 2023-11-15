25bad14796c62f55fc9afe8376b30c5b675d93e4
Paulo Gomes <139072768+PauloGomes1138@users.noreply.github.com>
2023-11-15 19:09:16 -0300

    Exercício Objetos
    Ações, tipos e propriedades

---
+++ script.js
@@ -0,0 +1,24 @@
+// escolher dois objetos materiais e dois objetos abstratos e atribua 3 métodos e 3 atributos a cada um 
+
+var meuCarro = new Object();
+meuCarro.fabricacao = "Wolks";
+meuCarro.modelo = "Gol";
+meuCarro.ano = 1990;
+
+// meuCarro.semPropriedade; //undefined - not so sure if the answer is right 
+
+
+var myHouse = new Object (); 
+myHouse.building = "still dont have it, but i will have";
+myHouse.model = "tinyhouse";
+myHouse.year = 2024; 
+myHouse.valueOf = isSecureContext; 
+
+// myHouse.semPropriedade; ou sem proprietário (risos)
+
+
+
+
+
+
+
